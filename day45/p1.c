49030d3a3ed0d4b5cfebff62c11726c30d872b55
Saranshichitra <Saranshi.chitra@gmail.com>
2026-03-19 22:47:23 +0530

    Add files via upload

---
+++ day45/p1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    newNode->data = value;    
+    newNode->left = NULL;      
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+int height(struct Node* root) {
+
+    if (root == NULL)
+        return 0;
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    if (leftHeight > rightHeight)
+        return leftHeight + 1;
+    else
+        return rightHeight + 1;
+}
+
+int main() {
+    struct Node* root = createNode(1);
+
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    int h = height(root);
+
+    printf("Height of tree = %d", h);
+
+    return 0;
+}
