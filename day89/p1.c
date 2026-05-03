784f2ced746515d9e0f7b2d7d933d0588ab8ad05
Saranshichitra <Saranshi.chitra@gmail.com>
2026-05-03 22:38:39 +0530

    Add files via upload

---
+++ day89/p1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int isPossible(int arr[], int n, int m, int maxPages) {
+    int students = 1;
+    int pagesSum = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > maxPages)
+            return 0;
+        if (pagesSum + arr[i] > maxPages) {
+            students++;
+            pagesSum = arr[i];
+            if (students > m)
+                return 0;
+        } else {
+            pagesSum += arr[i];
+        }
+    }
+    return 1;
+}
+
+int allocateBooks(int arr[], int n, int m) {
+    if (n < m)
+        return -1;
+    int sum = 0, max = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    int low = max, high = sum, result = sum;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (isPossible(arr, n, m, mid)) {
+            result = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return result;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("%d\n", allocateBooks(arr, n, m));
+    return 0;
+}
